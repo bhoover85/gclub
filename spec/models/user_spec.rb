81812d455369db5b00f6eb9b2c184fc111b8e5eb
Brad Hoover <hoov1185@gmail.com>
2014-05-07 10:59:40 -0400

    Added user spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
+require 'rspec/its'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+    @user = User.new(email: "user@example.com",
+                     password: "foobar1234", password_confirmation: "foobar1234")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
+
 end
