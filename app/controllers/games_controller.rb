5767540f73db4511a58b25472c860974d7fff989
Brad Hoover <hoov1185@gmail.com>
2014-05-14 09:13:33 -0400

    Starting games view.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,14 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_user, only: [:edit, :updat, :destroy]
+
+  def index
+    @games = Game.paginate(page: params[:page])
+  end
+
+  private
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users, :only => [:show]
+  resources :games
 
   root 'main#index'
 
