20be9475438d3a80a6ef55f61a2be5edccc84daf
Brad Hoover <hoov1185@gmail.com>
2014-05-30 10:21:57 -0400

    Created routing for wishlist. Fixed routing for admin.

--- config/routes.rb
+++ config/routes.rb
@@ -19,11 +19,11 @@ Rails.application.routes.draw do
   resources :wishlists, :only => [:create, :destroy]
 
   root 'main#index'
-  match '/admin',              to: 'admin#index', via: 'get'
-  match '/year/:year',         to: 'main#index',  via: 'get'
-  match '/users/:id/wishlist', to: 'users#wished_games',  via: 'get'
+  match '/year/:year',         to: 'main#index',          via: 'get'
 
-  get 'games/:id/edit' => 'games#edit', as: :edit
+  get 'games/:id/edit'     => 'games#edit', as: :edit
+  get 'users/:id/wishlist' => 'users#wished_games', as: :user_wishlist
+  get 'admin/'             => 'admin#index', as: :admin
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
