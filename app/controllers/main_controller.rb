f492a95e73b1bf94997f2ded1273ea54ac0f6e48
Brad Hoover <hoov1185@gmail.com>
2014-06-05 10:09:50 -0400

    Added filter title to main page. Randomize list when all games are being shown.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def index
-    @games = Game.filter(params.slice(:year))
+    @games = Game.filter(params.slice(:year)).order("RANDOM()").limit(5)
+    @filter = (params[:year].nil?) ? "All Time" : params[:year]
   end
 end
