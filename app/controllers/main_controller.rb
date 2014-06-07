846ea61813b909d06a066f804cecb30015fe118c
Brad Hoover <hoov1185@gmail.com>
2014-06-07 09:03:29 -0400

    Added pagination to main page.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
-    @games = Game.filter(params.slice(:year)).order("RANDOM()").limit(5)
+    @games = Game.filter(params.slice(:year)).order("RANDOM()").paginate(page: params[:page], :per_page => 10)
     @filter = (params[:year].nil?) ? "All Time" : params[:year]
   end
 end
