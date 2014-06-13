54580a076eb574421460156dd0119d12d61a3afa
Brad Hoover <hoov1185@gmail.com>
2014-06-13 08:41:17 -0400

    Added missing file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/var/rails/greatgames.club'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/application.yml config/schedule.rb}
+set :linked_files, %w{config/application.yml config/schedule.rb config/secrets.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
