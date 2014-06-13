55519050ce4739214dc9c65334b0af18f7fd0551
Brad Hoover <hoov1185@gmail.com>
2014-06-13 08:31:33 -0400

    Fixed linked_files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,8 +23,7 @@ set :deploy_to, '/var/rails/greatgames.club'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/application.yml}
-set :linked_files, %w{config/schedule.rb}
+set :linked_files, %w{config/application.yml config/schedule.rb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
