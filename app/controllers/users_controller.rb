d6425db65190aed50d942ecb29c8005a1c0b5dfd
Brad Hoover <hoov1185@gmail.com>
2014-06-17 10:21:41 -0400

    Restrict user index to admin only.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :admin_user, only: [:index]
   helper_method :sort_column, :sort_direction
 
   def index
