011e0d769bb42bfd5e2b29fb38c2ee25175ba10d
Brad Hoover <hoov1185@gmail.com>
2014-10-22 08:14:29 -0400

    Fixed Top Rated query

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   def index
     @suggested = Game.filter(params.slice(:platform)).order("RANDOM()").paginate(page: params[:page], :per_page => 6)
     @games_to_try = Game.filter(params.slice(:platform)).order("RANDOM()").paginate(page: params[:page], :per_page => 18)
-    @top_games = Game.order('score').limit(6).reverse
+    @top_games = Game.order('score desc').limit(6)
     @filter = (params[:platform].nil?) ? "All" : params[:platform]
   end
 end
