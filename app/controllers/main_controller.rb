ad3d9352bdb472f6b59617cb4af67ec171fcee00
Brad Hoover <hoov1185@gmail.com>
2014-10-22 13:20:49 -0400

    Added mid page ad

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,8 @@
 class MainController < ApplicationController
   def index
-    @suggested = Game.filter(params.slice(:platform)).order("RANDOM()").limit(6)
-    @games_to_try = Game.filter(params.slice(:platform)).order("RANDOM()").limit(18)
-    @top_games = Game.order('score desc').limit(6)
+    @suggested        = Game.filter(params.slice(:platform)).order("RANDOM()").limit(6)
+    @more_great_games = Game.filter(params.slice(:platform)).order("RANDOM()").limit(18)
+    @top_games        = Game.order('score desc').limit(6)
     # @filter = (params[:platform].nil?) ? "All" : params[:platform]
   end
 end
