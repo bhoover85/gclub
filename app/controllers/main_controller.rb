b0e10e151d4a67beb509a3832acd6723d473e8c3
Brad Hoover <hoov1185@gmail.com>
2014-10-22 09:10:32 -0400

    Cleaned up some active record queries

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,8 @@
 class MainController < ApplicationController
   def index
-    @suggested = Game.filter(params.slice(:platform)).order("RANDOM()").paginate(page: params[:page], :per_page => 6)
-    @games_to_try = Game.filter(params.slice(:platform)).order("RANDOM()").paginate(page: params[:page], :per_page => 18)
+    @suggested = Game.filter(params.slice(:platform)).order("RANDOM()").limit(6)
+    @games_to_try = Game.filter(params.slice(:platform)).order("RANDOM()").limit(18)
     @top_games = Game.order('score desc').limit(6)
-    @filter = (params[:platform].nil?) ? "All" : params[:platform]
+    # @filter = (params[:platform].nil?) ? "All" : params[:platform]
   end
 end
