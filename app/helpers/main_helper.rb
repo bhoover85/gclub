89984cd01476ea8841da1abf7f36aa40e4854128
Brad Hoover <hoov1185@gmail.com>
2014-10-22 10:14:08 -0400

    Hover colors

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,12 @@
 module MainHelper
+
+  def class_name game
+    if game.platform.parameterize == "3ds"
+      class_name = "threeds"
+    else
+      class_name = game.platform.parameterize
+    end
+
+    return class_name
+  end
 end
