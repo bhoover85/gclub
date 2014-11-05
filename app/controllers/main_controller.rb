00ec618fc751cc9b061ac0a5d47a23c460b15ec8
Brad Hoover <hoov1185@gmail.com>
2014-11-05 10:51:29 -0500

    Show 12 top rated games instead of 6

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   def index
     @suggested        = Game.filter(params.slice(:platform)).order("RANDOM()").limit(6)
     @more_great_games = Game.filter(params.slice(:platform)).order("RANDOM()").limit(18)
-    @top_games        = Game.order('score desc').limit(6)
+    @top_games        = Game.order('score desc').limit(12)
     # @filter = (params[:platform].nil?) ? "All" : params[:platform]
   end
 end
