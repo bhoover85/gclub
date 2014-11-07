7a01c1778285a8fe95a13465659700ddbdad00e3
Brad Hoover <hoov1185@gmail.com>
2014-11-07 09:12:37 -0500

    Fixed game reviews layout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,7 @@
 class MainController < ApplicationController
   def index
-    @suggested        = Game.filter(params.slice(:platform)).order("RANDOM()").limit(6)
-    @more_great_games = Game.filter(params.slice(:platform)).order("RANDOM()").limit(18)
     @top_games        = Game.order('score desc').limit(12)
+    @more_great_games = Game.order("RANDOM()").limit(18)
     # @filter = (params[:platform].nil?) ? "All" : params[:platform]
   end
 end
